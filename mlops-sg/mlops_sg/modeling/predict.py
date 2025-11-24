28592243e1edc892a4f055ae51a6c26f21494a4f
LJONeill <ljon.london@gmail.com>
2025-11-24 15:48:17 +0100

    feat: model deploy and staging

--- mlops-sg/mlops_sg/modeling/predict.py
+++ mlops-sg/mlops_sg/modeling/predict.py
@@ -83,5 +83,47 @@ else:
 
 print(f"Registered model: {run_id}")
 
+if run_id is not None:
+    print(f'Best model found: {run_id}')
+
+    model_uri = "runs:/{run_id}/{artifact_path}".format(
+        run_id=run_id,
+        artifact_path=artifact_path
+    )
+    model_details = mlflow.register_model(model_uri=model_uri, name=model_name)
+    wait_until_ready(model_details.name, model_details.version)
+    model_details = dict(model_details)
+    print(model_details)
+
+model_version = 1
+
+client = MlflowClient()
+
+
+def wait_for_deployment(model_name, model_version, stage='Staging'):
+    status = False
+    while not status:
+        model_version_details = dict(
+            client.get_model_version(name=model_name,version=model_version)
+            )
+        if model_version_details['current_stage'] == stage:
+            print(f'Transition completed to {stage}')
+            status = True
+            break
+        else:
+            time.sleep(2)
+    return status
+
+model_version_details = dict(client.get_model_version(name=model_name,version=model_version))
+model_status = True
+if model_version_details['current_stage'] != 'Staging':
+    client.transition_model_version_stage(
+        name=model_name,
+        version=model_version,stage="Staging", 
+        archive_existing_versions=True
+    )
+    model_status = wait_for_deployment(model_name, model_version, 'Staging')
+else:
+    print('Model already in staging')
 
 
