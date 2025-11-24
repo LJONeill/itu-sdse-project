3bb3c45a63d182fca5c2197041df0201fb64faec
LJONeill <ljon.london@gmail.com>
2025-11-24 14:35:14 +0100

    feat: xgboosted model performance metrics

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from mlops_sg.config import MODELS_DIR, PROCESSED_DATA_DIR
 from xgboost import XGBRFClassifier
 from scipy.stats import uniform
@@ -66,3 +67,31 @@ params = {
 model_grid = RandomizedSearchCV(model, param_distributions=params, n_jobs=-1, verbose=3, n_iter=10, cv=10)
 
 model_grid.fit(X_train, y_train)
+
+best_model_xgboost_params = model_grid.best_params_
+print("Best xgboost params")
+pprint(best_model_xgboost_params)
+
+y_pred_train = model_grid.predict(X_train)
+y_pred_test = model_grid.predict(X_test)
+print("Accuracy train", accuracy_score(y_pred_train, y_train ))
+print("Accuracy test", accuracy_score(y_pred_test, y_test))
+
+conf_matrix = confusion_matrix(y_test, y_pred_test)
+print("Test actual/predicted\n")
+print(pd.crosstab(y_test, y_pred_test, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
+print("Classification report\n")
+print(classification_report(y_test, y_pred_test),'\n')
+
+conf_matrix = confusion_matrix(y_train, y_pred_train)
+print("Train actual/predicted\n")
+print(pd.crosstab(y_train, y_pred_train, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
+print("Classification report\n")
+print(classification_report(y_train, y_pred_train),'\n')
+
+xgboost_model = model_grid.best_estimator_
+xgboost_model.save_model(model_path)
+
+model_results = {
+    model_path: classification_report(y_train, y_pred_train, output_dict=True)
+}
