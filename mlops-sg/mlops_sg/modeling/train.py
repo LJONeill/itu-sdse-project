f935af8073d3d93ae1c8fc5c3edb346d7eba13a1
LJONeill <ljon.london@gmail.com>
2025-11-24 14:27:34 +0100

    feat: perform xgboost

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -1,6 +1,9 @@
 from pathlib import Path
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from mlops_sg.config import MODELS_DIR, PROCESSED_DATA_DIR
+from xgboost import XGBRFClassifier
+from scipy.stats import uniform
+from scipy.stats import randint
 
 import datetime
 import mlflow
@@ -49,3 +52,17 @@ X = data.drop(["lead_indicator"], axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state=42, test_size=0.15, stratify=y)
+
+model = XGBRFClassifier(random_state=42)
+params = {
+    "learning_rate": uniform(1e-2, 3e-1),
+    "min_split_loss": uniform(0, 10),
+    "max_depth": randint(3, 10),
+    "subsample": uniform(0, 1),
+    "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
+    "eval_metric": ["aucpr", "error"]
+}
+
+model_grid = RandomizedSearchCV(model, param_distributions=params, n_jobs=-1, verbose=3, n_iter=10, cv=10)
+
+model_grid.fit(X_train, y_train)
