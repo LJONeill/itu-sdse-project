7be4a34e632f7421616084847cf5c07b223e185f
LJONeill <ljon.london@gmail.com>
2025-11-24 13:23:42 +0100

    feat: data drift dumped, training data saved to csv

--- mlops-sg/mlops_sg/dataset.py
+++ mlops-sg/mlops_sg/dataset.py
@@ -15,11 +15,12 @@ import joblib
 
 # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
 input_path: Path = RAW_DATA_DIR / "raw_data.csv",
-output_path: Path = PROCESSED_DATA_DIR / "processed_data.csv",
+training_data_path: Path = PROCESSED_DATA_DIR / "training_data.csv",
 date_limits_path: Path = INTERIM_DATA_DIR / "date_limits.json",
 outlier_summary_path: Path = INTERIM_DATA_DIR / "outlier_summary.csv",
 cat_missing_impute_path: Path = INTERIM_DATA_DIR / "cat_missing_impute.csv",
-scaler_path: Path = EXTERNAL_DATA_DIR / "scaler.pkl"
+scaler_path: Path = EXTERNAL_DATA_DIR / "scaler.pkl",
+column_drift_path: Path = INTERIM_DATA_DIR / "columns_drift.json"
 
 
 # Defined variables for use throughout
@@ -134,4 +135,10 @@ cont_vars = cont_vars.reset_index(drop=True)
 cat_vars = cat_vars.reset_index(drop=True)
 data = pd.concat([cat_vars, cont_vars], axis=1)
 print(f"Data cleansed and combined.\nRows: {len(data)}")
+
+data_columns = list(data.columns)
+with open(column_drift_path,'w+') as f:           
+    json.dump(data_columns,f)
+    
+data.to_csv(training_data_path, index=False)
 
