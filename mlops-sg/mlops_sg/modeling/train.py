496efd63d7f280e279f6ac25a4ffb7a2977733fe
LJONeill <ljon.london@gmail.com>
2025-11-24 14:20:39 +0100

    feat: read in feature data, split by type

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -7,7 +7,7 @@ import mlflow
 import pandas as pd
 
 # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-training_gold_path: Path = PROCESSED_DATA_DIR / "training_gold.csv",
+data_gold_path: Path = PROCESSED_DATA_DIR / "training_gold.csv",
 features_path: Path = PROCESSED_DATA_DIR / "features.csv",
 labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
 model_path: Path = MODELS_DIR / "model.pkl",
@@ -20,11 +20,26 @@ def create_dummy_cols(df, col):
     return new_df
 
 current_date = datetime.datetime.now().strftime("%Y_%B_%d")
-data_gold_path = training_gold_path
 data_version = "00000"
 experiment_name = current_date
 
 mlflow.set_experiment(experiment_name)
 
+data = pd.read_csv(data_gold_path)
 
+data = data.drop(["lead_id", "customer_code", "date_part"], axis=1)
 
+cat_cols = ["customer_group", "onboarding", "bin_source", "source"]
+cat_vars = data[cat_cols]
+
+other_vars = data.drop(cat_cols, axis=1)
+
+for col in cat_vars:
+    cat_vars[col] = cat_vars[col].astype("category")
+    cat_vars = create_dummy_cols(cat_vars, col)
+
+data = pd.concat([other_vars, cat_vars], axis=1)
+
+for col in data:
+    data[col] = data[col].astype("float64")
+    print(f"Changed column {col} to float")
