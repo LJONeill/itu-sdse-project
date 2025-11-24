d403dfad98a51827d3e54203ca6445dfef03ea55
LJONeill <ljon.london@gmail.com>
2025-11-24 14:14:09 +0100

    feat: imported feature data and set up mlflow experiment

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -2,9 +2,19 @@ from pathlib import Path
 
 from mlops_sg.config import MODELS_DIR, PROCESSED_DATA_DIR
 
+import datetime
+import mlflow
+
 # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+training_gold_path: Path = PROCESSED_DATA_DIR / "training_gold.csv",
 features_path: Path = PROCESSED_DATA_DIR / "features.csv",
 labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
 model_path: Path = MODELS_DIR / "model.pkl",
 
+current_date = datetime.datetime.now().strftime("%Y_%B_%d")
+data_gold_path = training_gold_path
+data_version = "00000"
+experiment_name = current_date
+
+mlflow.set_experiment(experiment_name)
 
