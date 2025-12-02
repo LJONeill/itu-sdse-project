72523c1af96280f1c479fb758f3919d085ee15c1
LJONeill <ljon.london@gmail.com>
2025-12-02 16:53:16 +0100

    docs: moved some data manipulation from train to features, commented it too

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -22,14 +22,6 @@ lr_model_path: Path = MODELS_DIR / "lr_model.pkl"
 column_list_path: Path = MODELS_DIR / "columns_list.json"
 model_results_path: Path = MODELS_DIR /  "model_results.json"
 
-
-# Defined functions for use throughout
-def create_dummy_cols(df, col):
-    df_dummies = pd.get_dummies(df[col], prefix=col, drop_first=True)
-    new_df = pd.concat([df, df_dummies], axis=1)
-    new_df = new_df.drop(col, axis=1)
-    return new_df
-
 # Define experiment name
 current_date = datetime.datetime.now().strftime("%Y_%B_%d")
 data_version = "00000"
@@ -40,23 +32,6 @@ mlflow.set_experiment(experiment_name)
 
 # Load the data
 data = pd.read_csv(data_gold_path)
-
-data = data.drop(["lead_id", "customer_code", "date_part"], axis=1)
-
-cat_cols = ["customer_group", "onboarding", "bin_source", "source"]
-cat_vars = data[cat_cols]
-
-other_vars = data.drop(cat_cols, axis=1)
-
-for col in cat_vars:
-    cat_vars[col] = cat_vars[col].astype("category")
-    cat_vars = create_dummy_cols(cat_vars, col)
-
-data = pd.concat([other_vars, cat_vars], axis=1)
-
-for col in data:
-    data[col] = data[col].astype("float64")
-    print(f"Changed column {col} to float")
 
 y = data["lead_indicator"]
 X = data.drop(["lead_indicator"], axis=1)
