21595f83f09db825dc3552a727fed49a445d1f7c
LJONeill <ljon.london@gmail.com>
2025-12-03 10:33:01 +0100

    docs(train): added comments, trimmed prints

--- mlops-sg/mlops_sg/modeling/train.py
+++ mlops-sg/mlops_sg/modeling/train.py
@@ -17,7 +17,7 @@ import json
 data_gold_path: Path = PROCESSED_DATA_DIR / "training_gold.csv"
 features_path: Path = PROCESSED_DATA_DIR / "features.csv"
 labels_path: Path = PROCESSED_DATA_DIR / "labels.csv"
-model_path: Path = MODELS_DIR / "model.pkl"
+xgboost_model_path: Path = MODELS_DIR / "xgboost_model.pkl"
 lr_model_path: Path = MODELS_DIR / "lr_model.pkl"
 column_list_path: Path = MODELS_DIR / "columns_list.json"
 model_results_path: Path = MODELS_DIR /  "model_results.json"
@@ -44,7 +44,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Define xgboost model
 model = XGBRFClassifier(random_state=42)
 
-# Define parameter possibilities
+# Define hyperparameter possibilities
 params = {
     "learning_rate": uniform(1e-2, 3e-1),
     "min_split_loss": uniform(0, 10),
@@ -54,40 +54,25 @@ params = {
     "eval_metric": ["aucpr", "error"]
 }
 
-# Define grid search with given xgboost model and parameters
+# Define grid search with given xgboost model and hyperparameters
 model_grid = RandomizedSearchCV(model, param_distributions=params, n_jobs=-1, verbose=3, n_iter=10, cv=10)
 
 # Perform grid search
 model_grid.fit(X_train, y_train)
 
-# Store best fit parameters
+# Extract best fit hyperparameters (only print)
 best_model_xgboost_params = model_grid.best_params_
 
-
-y_pred_train = model_grid.predict(X_train)
-y_pred_test = model_grid.predict(X_test)
-print("Accuracy train", accuracy_score(y_pred_train, y_train ))
-print("Accuracy test", accuracy_score(y_pred_test, y_test))
-
-conf_matrix = confusion_matrix(y_test, y_pred_test)
-print("Test actual/predicted\n")
-print(pd.crosstab(y_test, y_pred_test, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-print("Classification report\n")
-print(classification_report(y_test, y_pred_test),'\n')
-
-conf_matrix = confusion_matrix(y_train, y_pred_train)
-print("Train actual/predicted\n")
-print(pd.crosstab(y_train, y_pred_train, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-print("Classification report\n")
-print(classification_report(y_train, y_pred_train),'\n')
-
+# Output the best xgboost model
 xgboost_model = model_grid.best_estimator_
-xgboost_model.save_model(model_path)
+xgboost_model.save_model(xgboost_model_path)
 
+# Initiate set of all model results and add xgboost results
 model_results = {
-    model_path: classification_report(y_train, y_pred_train, output_dict=True)
+    xgboost_model_path: classification_report(y_train, y_pred_train, output_dict=True)
 }
 
+# Create a class with a predict function to show model results
 class lr_wrapper(mlflow.pyfunc.PythonModel):
     def __init__(self, model):
         self.model = model
@@ -95,10 +80,13 @@ class lr_wrapper(mlflow.pyfunc.PythonModel):
     def predict(self, context, model_input):
         return self.model.predict_proba(model_input)[:, 1]
 
-
+# Initiate experiment tracking
 mlflow.sklearn.autolog(log_input_examples=True, log_models=False)
+
+# Initiate experiment
 experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
+# run logistic regression model training
 with mlflow.start_run(experiment_id=experiment_id) as run:
     model = LogisticRegression()
 
@@ -127,38 +115,18 @@ with mlflow.start_run(experiment_id=experiment_id) as run:
     # Custom python model for predicting probability 
     mlflow.pyfunc.log_model('model', python_model=lr_wrapper(model))
 
-
+# Generate model report stats
 model_classification_report = classification_report(y_test, y_pred_test, output_dict=True)
 
-best_model_lr_params = model_grid.best_params_
-
-print("Best lr params")
-pprint(best_model_lr_params)
-
-print("Accuracy train:", accuracy_score(y_pred_train, y_train ))
-print("Accuracy test:", accuracy_score(y_pred_test, y_test))
-
-conf_matrix = confusion_matrix(y_test, y_pred_test)
-print("Test actual/predicted\n")
-print(pd.crosstab(y_test, y_pred_test, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-print("Classification report\n")
-print(classification_report(y_test, y_pred_test),'\n')
-
-conf_matrix = confusion_matrix(y_train, y_pred_train)
-print("Train actual/predicted\n")
-print(pd.crosstab(y_train, y_pred_train, rownames=['Actual'], colnames=['Predicted'], margins=True),'\n')
-print("Classification report\n")
-print(classification_report(y_train, y_pred_train),'\n')
-
+# Store model report stats to earlier created set
 model_results[lr_model_path] = model_classification_report
-print(model_classification_report["weighted avg"]["f1-score"])
 
+# Store training data
 with open(column_list_path, 'w+') as columns_file:
     columns = {'column_names': list(X_train.columns)}
     print(columns)
     json.dump(columns, columns_file)
 
-print('Saved column list to ', column_list_path)
-
+# Store model results
 with open(model_results_path, 'w+') as results_file:
     json.dump(model_results, results_file)
