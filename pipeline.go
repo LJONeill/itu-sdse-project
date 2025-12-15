2d0260523a24da13269b377d08f23263314b8ac1
luke <ljon.london@gmail.com>
2025-12-15 13:02:00 +0100

    test: updated pipeline to encompass dataset and config py files

--- pipeline.go
+++ pipeline.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"dagger.io/dagger"
 )
@@ -9,18 +10,14 @@ import (
 func main() {
 	ctx := context.Background()
 
-	Build()
-	data_pipeline()
+	if err := Build(ctx); err != nil {
+        fmt.Println("Error:", err)
+        panic(err)
+    }
 	//ml_pipeline()
-
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func Build(ctx context.Context) error {
-	
 	// Initialize Dagger client
 	client, err := dagger.Connect(ctx)
 	if err != nil {
@@ -29,48 +26,27 @@ func Build(ctx context.Context) error {
 	defer client.Close()
 
 	python := client.Container().From("python:3.12.2-bookworm").
-		WithDirectory("itu-forked-project", client.Host().Directory("src")).
+		WithDirectory("py_files", client.Host().Directory("src")).
 		WithExec([]string{"python", "--version"})
 
-	python = python.WithExec([]string{"python", "config.py"})
+	python = python.WithExec([]string{"python", "py_files/config.py"})
 
 	_, err = python.
-		Directory("build_output"). // Before writing to this folder, we may have to make sure it exists, 'os.makedirs('build_output', exist_ok=True)' 
-		Export(ctx, "build_output") //the exist_ok=True makes nothing happen if it already exists
+		Directory("output"). // Before writing to this folder, we may have to make sure it exists, 'os.makedirs('output', exist_ok=True)' 
+		Export(ctx, "output") //the exist_ok=True makes nothing happen if it already exists
 	if err != nil {
 		return err
 	}
-	return nil
-}
 
-func data_pipeline() {
-	
-	data_pipeline := client.Container().From("python:3.12.2-bookworm").
-		WithDirectory("itu-forked-project", client.Host().Directory("src")).
-		WithExec([]string{"python", "dataset.py"})
+	data := client.Container().From("python:3.12.2-bookworm").
+		WithDirectory("py_files", client.Host().Directory("src")).
+		WithExec([]string{"python", "py_files/dataset.py"})
 
-	//data_pipeline = data_pipeline.WWithExec([]string{"python", "features.py"})
+	//data = data.WWithExec([]string{"python", "features.py"})
 
-	_, err = data_pipeline.
-		Directory("data_pipeline_output"). // See above re: folder creation
-		Export(ctx, "data_pipeline_output")
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func ml_pipeline() {
-	
-	ml_pipeline := client.Container().From("python:3.12.2-bookworm").
-		WithDirectory("itu-forked-project", client.Host().Directory("src/modelling")).
-		WithExec([]string{"python", "train.py"})
-		
-	ml_pipeline = ml_pipeline.WWithExec([]string{"python", "model_selection.py"})
-
-	_, err = ml_pipeline.
-		Directory("ml_pipeline_output"). // See above re: folder creation
-		Export(ctx, "ml_pipeline_output")
+	_, err = data.
+		Directory("output"). // See above re: folder creation
+		Export(ctx, "output")
 	if err != nil {
 		return err
 	}
