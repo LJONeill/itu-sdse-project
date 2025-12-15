944cef4ebf3562db60c46badf00fa67ae74b3dee
luke <ljon.london@gmail.com>
2025-12-15 12:27:20 +0100

    test: edited dagger workflow to prepare for test with config and dataset py files

--- pipeline.go
+++ pipeline.go
@@ -11,10 +11,12 @@ func main() {
 
 	Build()
 	data_pipeline()
-	ml_pipeline()
+	//ml_pipeline()
 
-	
-
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func Build(ctx context.Context) error {
@@ -30,15 +32,14 @@ func Build(ctx context.Context) error {
 		WithDirectory("itu-forked-project", client.Host().Directory("src")).
 		WithExec([]string{"python", "--version"})
 
-	python = python.WithExec([]string{"python", "__init__.py"})
+	python = python.WithExec([]string{"python", "config.py"})
 
 	_, err = python.
-		Directory("output")
-		Export(ctx, "output")
+		Directory("build_output"). // Before writing to this folder, we may have to make sure it exists, 'os.makedirs('build_output', exist_ok=True)' 
+		Export(ctx, "build_output") //the exist_ok=True makes nothing happen if it already exists
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
@@ -48,12 +49,15 @@ func data_pipeline() {
 		WithDirectory("itu-forked-project", client.Host().Directory("src")).
 		WithExec([]string{"python", "dataset.py"})
 
+	//data_pipeline = data_pipeline.WWithExec([]string{"python", "features.py"})
+
 	_, err = data_pipeline.
-		Directory("output")
-		Export(ctx, "output")
+		Directory("data_pipeline_output"). // See above re: folder creation
+		Export(ctx, "data_pipeline_output")
 	if err != nil {
 		return err
 	}
+	return nil
 }
 
 func ml_pipeline() {
@@ -65,9 +69,10 @@ func ml_pipeline() {
 	ml_pipeline = ml_pipeline.WWithExec([]string{"python", "model_selection.py"})
 
 	_, err = ml_pipeline.
-		Directory("output")
-		Export(ctx, "output")
+		Directory("ml_pipeline_output"). // See above re: folder creation
+		Export(ctx, "ml_pipeline_output")
 	if err != nil {
 		return err
 	}
+	return nil
 }
